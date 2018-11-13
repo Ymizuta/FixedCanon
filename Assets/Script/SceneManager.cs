f630a8c1302c0caa94cdb7e3b42e27bd27026893
Ymizuta <suiden.53@gmail.com>
2018-11-14 03:13:36 +0900

    [add]SceneManagerを実装。テスト用のスタブモジュール(SceneBase,TitleScene,StageScene)を追加。

--- Assets/Script/SceneManager.cs
+++ Assets/Script/SceneManager.cs
@@ -6,24 +6,42 @@ public class SceneManager : MonoBehaviour {
 
     private SceneBase current_scene_;
 
+    public void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {          
+            NextScene("TitleScene",null);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            NextScene("StageScene", null);
+        }
+    }
+
     private void Init()
     {
-
+        
     }
 
     public void NextScene(string scene_name,object scene_params)
     {
-        return;
+        RemoveScene(current_scene_);
+        AddScene(scene_name,scene_params);
     }
 
     private void AddScene(string scene_name, object scene_params)
     {
-        return;
+        string className_ = scene_name;
+        System.Type type_ = System.Type.GetType(className_);
+        current_scene_ = (SceneBase)System.Activator.CreateInstance(type_);        
     }
 
-    private void RemoveScene(string scene_name)
+    private void RemoveScene(SceneBase scene_)
     {
-        return;
+        Debug.Log(current_scene_ + "削除");
+        current_scene_ = null;
     }
 
 }
