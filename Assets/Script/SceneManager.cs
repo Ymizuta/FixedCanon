1f134992181a53664199a913207a5dac42f90303
Ymizuta <suiden.53@gmail.com>
2018-11-14 01:58:06 +0900

    [add]SceneManager.csを追加(メソッド未実装)

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    private SceneBase current_scene_;
+
+    private void Init()
+    {
+
+    }
+
+    public void NextScene(string scene_name,object scene_params)
+    {
+        return;
+    }
+
+    private void AddScene(string scene_name, object scene_params)
+    {
+        return;
+    }
+
+    private void RemoveScene(string scene_name)
+    {
+        return;
+    }
+
+}
