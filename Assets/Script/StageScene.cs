e9a2608c75b52c44c5db6ffe0fed09332bd25291
Ymizuta <suiden.53@gmail.com>
2018-11-16 01:09:04 +0900

    [add]StageSceneクラス(テスト用ドライバモジュール)を追加(未実装)

---
+++ Assets/Script/StageScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScene : SceneBase {
+
+    /// <summary>
+    /// テスト用ドライバモジュール
+    /// </summary>
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
