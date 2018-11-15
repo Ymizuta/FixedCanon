138de44bbcc569238a397bc30949fa81eaa9502c
Ymizuta <suiden.53@gmail.com>
2018-11-16 01:03:43 +0900

    [add]StateBaseクラス、StageMainStateクラスを準備(未実装)

---
+++ Assets/Script/SceneBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SceneBase : MonoBehaviour {
+
+    /// <summary>
+    /// ドライバモジュール
+    /// </summary>
+
+    private StateBase current_state_;
+
+    private void Init()
+    {
+
+    }
+}
---
+++ Assets/Script/StateBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateBase : MonoBehaviour {
+
+    public SceneBase scene_;
+
+    private　void Init()
+    {
+
+    }
+}
