328e8f3f16cedf4bc93676ec19c9c435fa3f3660
Ymizuta <suiden.53@gmail.com>
2018-11-18 06:54:42 +0900

    [add]砲弾の抽象クラスBulletBaseを作成

--- Assets/Script/BulletBase.cs
+++ Assets/Script/BulletBase.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBase : MonoBehaviour {
+public abstract class BulletBase : MonoBehaviour {
+    public float damage_;
+    public float shoot_power_;
 
+    public void Init()
+    {
 
+    }
 }
--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
+    [SerializeField] GameObject muzzle_;
+
     public void Shoot(BulletBase bullet)
     {
         Debug.Log(bullet + "を発射");
         BulletBase bullet_clone = Instantiate(bullet);
-        bullet_clone.GetComponent<Rigidbody>().AddForce();
+        bullet_clone.GetComponent<Rigidbody>().AddForce(muzzle_.transform.forward * bullet.shoot_power_);
         Destroy(bullet_clone,2.0f);
     }
 
