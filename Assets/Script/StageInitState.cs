4ee4d26a3489941e08a471df93ddbc274e0ac736
Ymizuta <suiden.53@gmail.com>
2018-11-18 01:33:36 +0900

    [modify]SceneManagerからSceneを呼び出す処理を修正

---
+++ Assets/Script/StageInitState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageInitState : StateBase {
+
+    /// <summary>
+    /// スタブモジュール
+    /// </summary>
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void Init()
+    {
+        Debug.Log("初期化処理");
+    }
+
+    private void ChangeState()
+    {
+        scene_.ChangeState(StateList.StageMainState);
+    }
+
+}
