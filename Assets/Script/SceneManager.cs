61ff1da36148d60836a7019dceac4a0eb5bc1ee2
Ymizuta <suiden.53@gmail.com>
2018-11-17 22:16:07 +0900

    [add]adding SceneManager.

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    SceneBase current_scene_;
+    [SerializeField] SceneBase stage_scene_ = null;
+
+	// Use this for initialization
+	void Start () {
+        current_scene_ = Instantiate(stage_scene_);
+	}
+}
