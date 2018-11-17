5b3ae28a76f81062b4da05efc97cf104b44fb9fa
Ymizuta <suiden.53@gmail.com>
2018-11-18 06:47:35 +0900

    [add]砲弾を発射する処理を実装

--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -7,6 +7,9 @@ public class Shooter : MonoBehaviour {
     public void Shoot(BulletBase bullet)
     {
         Debug.Log(bullet + "を発射");
+        BulletBase bullet_clone = Instantiate(bullet);
+        bullet_clone.GetComponent<Rigidbody>().AddForce();
+        Destroy(bullet_clone,2.0f);
     }
 
 }
