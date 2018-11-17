6f505617b83f2f9c160c9309ac42363a059cc4dd
Ymizuta <suiden.53@gmail.com>
2018-11-17 16:12:48 +0900

    [add]移動処理の流れを追加(未実装)

---
+++ Assets/Script/CanonMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonMove : MonoBehaviour {
+
+    public void Move()
+    {
+        Debug.Log("Move");
+    }
+
+}
--- Assets/Script/StageMainState.cs
+++ Assets/Script/StageMainState.cs
@@ -8,7 +8,7 @@ public class StageMainState : StateBase {
     [SerializeField] Shooter shooter_;
     [SerializeField] BulletChanger bullet_changer_;
     [SerializeField] BulletCounter bullet_counter_;
-    [SerializeField] Move move_;
+    [SerializeField] CanonMove canon_move_;
 
     private void Start()
     {
@@ -16,12 +16,7 @@ public class StageMainState : StateBase {
 
     private void Update()
     {
-        //ユーザ操作を受けて(UserOperationクラスで実装)
-        if (Input.GetButtonDown("Horizontal"))
-        {
-            //角度変更
-            //move_.Move(direction,speed)
-        }
+        //砲弾発射
         //ユーザ操作を受けて(UserOperationクラスで実装)
         if (Input.GetMouseButtonDown(0))
         {
@@ -32,11 +27,30 @@ public class StageMainState : StateBase {
             //弾数確認
             bullet_counter_.BulletCount(player_params_);
         }
+
+        //砲弾変更
         //ユーザ操作を受けて(UserOperationクラスで実装)
         if (Input.GetMouseButtonDown(1))
         {
             //砲弾の種類変更
             bullet_changer_.ChangeBullet(player_params_);
+        }
+
+        //角度変更
+        //ユーザ操作を受けて(UserOperationクラスで実装)
+        TouchInfo info = UserOperation.GetTouch();
+        //canon_move_.Move(direction,speed)
+        if (info == TouchInfo.Began)
+        {
+            //CanonMove.Move(vector3 ,speed );
+        }
+        else if (info == TouchInfo.Moved)
+        {
+            //CanonMove.Move(vector3 ,speed );
+        }
+        else if (info == TouchInfo.Ended)
+        {
+            //処理あれば
         }
     }
 
@@ -51,6 +65,5 @@ public class StageMainState : StateBase {
     {
         //ターゲット数カウント
         //ターゲット数が０の場合コールバックしてクリア
-
     }
 }
