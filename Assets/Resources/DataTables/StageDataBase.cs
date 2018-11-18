b32b9ef4614b7203861a21e6a1d70a4b6def7c93
Ymizuta <suiden.53@gmail.com>
2018-11-18 13:49:44 +0900

    [add]ステージ毎のパラメータテーブル作成のセットアップ

---
+++ Assets/Resources/DataTables/StageDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageDB", menuName = "MyGame/Create StageDB")]
+public class StageDataBase : ScriptableObject {
+
+    [SerializeField]
+    private List<StageParamsTable> stage_params_table_list = new List<StageParamsTable>();
+
+    //リストを返す
+    public List<StageParamsTable> GetStageParamasList()
+    {
+        return stage_params_table_list;
+    }
+}
---
+++ Assets/Resources/DataTables/StageParamsTable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageParams", menuName = "MyGame/Create StageParams")]
+public class StageParamsTable : ScriptableObject{
+
+    [SerializeField] int stage_id;
+    [SerializeField] BulletBase[] useable_bullet;
+    [SerializeField] GameObject[] stage_object;
+}
