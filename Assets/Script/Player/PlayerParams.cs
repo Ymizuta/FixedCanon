3e117368797b9bda1c8b6f8a47e8d974678696a3
Ymizuta <suiden.53@gmail.com>
2018-11-24 00:25:51 +0900

    [modify]弾数パラメータの初期化処理を修正

--- Assets/Script/Player/PlayerParams.cs
+++ Assets/Script/Player/PlayerParams.cs
@@ -8,7 +8,7 @@ public class PlayerParams : MonoBehaviour
     private BulletBase[] bullets_ = null;               //砲弾のインスタンス
     private int[] number_of_bullet_;                    //砲弾の弾数
     private BulletBase loadedbullet_;                   //現在装備している砲弾
-    private int bullet_index_;                          //砲弾のプレハブ/弾数のリストのインデックス
+    private int bullet_index_;                          //砲弾のプレハブ/弾数のリストのインデックス(装備砲弾の変更時にインデックスを前に進める)
     private int default_bullet_index_ = 0;              //初期のインデックス
     private readonly string BULLET_PATH = "Bullet/";    //Resourcesフォルダ以下のパス
 
@@ -49,24 +49,21 @@ public class PlayerParams : MonoBehaviour
     //パラメータの初期化
     public void InitParams(StageInfo stage_info)
     {
-        bullet_index_ = default_bullet_index_;
-
         bullets_obj_ = new GameObject[stage_info.bullet_type.Length];
-        //ステージで利用可能な砲弾を取得
-        for(int bullet_i = 0; bullet_i < stage_info.bullet_type.Length; bullet_i++)
-        {
-            bullets_obj_[bullet_i] = Resources.Load(BULLET_PATH + stage_info.bullet_type[bullet_i]) as GameObject;
-            bullets_[bullet_i] = (BulletBase)bullets_obj_[bullet_i].GetComponent(System.Type.GetType(stage_info.bullet_type[bullet_i]));
-        }
-
-        //弾数初期設定
+        bullets_ = new BulletBase[bullets_obj_.Length];
         number_of_bullet_ = new int[bullets_.Length];
-        int i;
-        for (i = 0; i < number_of_bullet_.Length; i++)
+        //ステージで利用可能な砲弾を取得
+        for(int i = 0; i < stage_info.bullet_type.Length; i++)
         {
-            number_of_bullet_[i] = 5;
+            //Bulletのプレハブオブジェクトを取得
+            bullets_obj_[i] = Resources.Load(BULLET_PATH + stage_info.bullet_type[i]) as GameObject;
+            //Bulletの種類ごとのコンポーネントを取得
+            bullets_[i] = (BulletBase)bullets_obj_[i].GetComponent(System.Type.GetType(stage_info.bullet_type[i]));
+            //各砲弾の弾数設定
+            number_of_bullet_[i] = stage_info.number_of_bullet[i];
         }
         //初期装備の弾を装備      
+        bullet_index_ = default_bullet_index_;
         loadedbullet_ = bullets_[bullet_index_];
     }
 
