5b4c5da9c875ecbdf1ae630c9d136581d1820bfe
Ymizuta <suiden.53@gmail.com>
2018-11-23 18:15:12 +0900

    [add]ターゲットが残っているかを判定する処理を追加

--- Assets/Script/StageState/StageInitStateScripts/ObjectParams.cs
+++ Assets/Script/StageState/StageInitStateScripts/ObjectParams.cs
@@ -6,29 +6,29 @@ public class ObjectParams : MonoBehaviour {
 
     [SerializeField] GameObject stage_object_ = null;       //
     private GameObject stage_object_clone_ = null;          //
-    private List<GameObject> target_object_list;            //
+    private List<GameObject> target_object_list_;            //
 
     public void Init(GameObject stage_object_clone)
     {
         stage_object_clone_ = stage_object_clone;
 
-        target_object_list = new List<GameObject>();
+        target_object_list_ = new List<GameObject>();
         //ステージオブジェクトの子オブジェクトの中からターゲットオブジェクトのみ取得
         foreach (Transform child in stage_object_clone_.transform)
         {
             //TargetObjectのタグが付いているものだけ取得
             if (child.tag == StageObjectList.TargetObject)
             {
-                target_object_list.Add(child.gameObject);
+                target_object_list_.Add(child.gameObject);
                 //Debug.Log(child.gameObject);
             }
         }
-        //ターゲットオブジェクトにコールバック関数を登録
-        for (int i = 0; i < target_object_list.Count; i++)
-        {
-            //Debug.Log(i + "番目は"+target_object_list[i]);
-            target_object_list[i].GetComponent<TargetObject>().OnTargetObjectDie += OnTargetObjectDieCallBack;
-        }
+        ////ターゲットオブジェクトにコールバック関数を登録
+        //for (int i = 0; i < target_object_list_.Count; i++)
+        //{
+        //    //Debug.Log(i + "番目は"+target_object_list[i]);
+        //    target_object_list_[i].GetComponent<TargetObject>().OnTargetObjectDie += OnTargetObjectDieCallBack;
+        //}
     }
 
     public GameObject StageObject
@@ -39,13 +39,25 @@ public class ObjectParams : MonoBehaviour {
         }
     }
 
+    public List<GameObject> TargetObjectList
+    {
+        get
+        {
+            return target_object_list_;
+        }
+    }
+
     //ターゲットオブジェクトが破壊された際に呼び出し
     private void OnTargetObjectDieCallBack()
     {
-        Debug.Log("ターゲットが一つ死亡");
+        //for (int i = 0; i < target_object_list.Count; i++)
+        //{
+        //    if (target_object_list[i] == null)
+        //    {
+        //        Debug.Log(i + "番目は破壊されています");
+        //    }
+        //}
     } 
-
-
     //public GameObject StageObjectClone
     //{
     //    get
---
+++ Assets/Script/TargetObjectCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObjectCounter : MonoBehaviour {
+
+    public bool ExistTargetObjects(List<GameObject> target_object_list)
+    {
+        for (int i = 0; i < target_object_list.Count; i++)
+        {
+            if (target_object_list[i] != null)
+            {
+                //Debug.Log(target_object_list[i] + "が生きています");
+                break;
+            }
+            if (i == target_object_list.Count -1)
+            {
+            //Debug.Log("ターゲットは全滅しています");
+            return false;
+            }
+        }
+        //Debug.Log("ターゲットは全滅していません");
+        return true;
+    }
+}
