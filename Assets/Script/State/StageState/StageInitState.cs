30b18704ff28ea3d51edbfd315b5987f295e5154
Ymizuta <suiden.53@gmail.com>
2018-11-25 01:47:00 +0900

    [remove]ObjectCreatorクラスを削除しStageInitStateに処理を集約

--- Assets/Script/State/StageState/StageInitState.cs
+++ Assets/Script/State/StageState/StageInitState.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class StageInitState : StateBase {
 
-    private ObjectCreator object_creator_;
-    public GameObject stage_object_;
-
 	// Use this for initialization
 	void Start () {
         Init();
-        object_creator_ = this.GetComponent<ObjectCreator>();
         //オブジェクトパラムスにステージオブジェクトのクローンを渡す
-        //((StageScene)scene_).ObjParams.StageObjectClone 
-        //    =  object_creator_.CreateObject(((StageScene)scene_).ObjParams.StageObject);
-        GameObject stage_object_clone = object_creator_.CreateObject(((StageScene)scene_).ObjParams.StageObject);
+        GameObject stage_object_clone = Instantiate(((StageScene)scene_).ObjParams.StageObject);
         ((StageScene)scene_).ObjParams.Init(stage_object_clone);
-
+        //ステート移行
         scene_.ChangeState(StateList.StageMainState,null);
     }    
-
 }
