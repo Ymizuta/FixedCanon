cc41744c3567cee0bd3bf0f17ee771729ff50958
Ymizuta <suiden.53@gmail.com>
2018-11-25 12:15:48 +0900

    [add]StageObjectManagerを追加

--- Assets/Script/StageObjectManager.cs
+++ Assets/Script/StageObjectManager.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class StageObjectManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private StageObjectParams params_;
+    private TargetObjectCounter target_obj_counter;
+
+    public void Setup()
+    {
+        params_ = this.GetComponent<StageObjectParams>();
+        target_obj_counter = this.GetComponent<TargetObjectCounter>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public StageObjectParams Params
+    {
+        get
+        {
+            return params_;
+        }
+    }
+
+    public TargetObjectCounter TargetObjectCounter
+    {
+        get
+        {
+            return target_obj_counter;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        params_ = null;
+        target_obj_counter = null;
+    }
 }
