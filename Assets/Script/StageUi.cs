208e34b6513efdd439f9dd0ce1e1a81c1f48ac69
Ymizuta <suiden.53@gmail.com>
2018-12-07 22:19:22 +0900

    [add]StageUiクラスを追加（処理未実装）

--- Assets/Script/StageUi.cs
+++ Assets/Script/StageUi.cs
@@ -4,13 +4,84 @@ using UnityEngine;
 
 public class StageUi : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject clear_ui_;
+    private GameObject game_over_ui_;    
+    private GameObject next_stage_button_;
+    private GameObject to_stage_select_button_;
+    private GameObject retry_button_;
+    
+    public GameObject ClearUi
+    {
+        get
+        {
+            return clear_ui_;
+        }
+        set
+        {
+            clear_ui_ = value;
+        }
+    }
+
+    public GameObject GameOverUi
+    {
+        get
+        {
+            return game_over_ui_;
+        }
+        set
+        {
+            game_over_ui_ = value;
+        }
+    }
+
+    public GameObject NextStageButton
+    {
+        get
+        {
+            return next_stage_button_;
+        }
+        set
+        {
+            next_stage_button_ = value;
+        }
+    }
+
+    public GameObject ToStageSelectButton
+    {
+        get
+        {
+            return to_stage_select_button_;
+        }
+        set
+        {
+            to_stage_select_button_ = value;
+        }
+    }
+
+    public GameObject RetryButton
+    {
+        get
+        {
+            return retry_button_;
+        }
+        set
+        {
+            retry_button_ = value;
+        }
+    }
+
+    public void PushNextStageButton()
+    {
+
+    }
+
+    public void PushToStageSelectButton()
+    {
+
+    }
+
+    public void PushRetryButton()
+    {
+
+    }
 }
