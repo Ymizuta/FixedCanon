294a320a674f393e1392525b9d5fb826d782a8be
Ymizuta <suiden.53@gmail.com>
2019-01-03 01:09:31 +0900

    [add]任意のオブジェクトの周囲を回転させるためのスクリプトを作成

---
+++ Assets/Script/StageObject/ObjectAction/RotationAround.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAround : MonoBehaviour {
+
+    [SerializeField] GameObject rotate_center_obj_;
+    [SerializeField] float rotate_speed_;
+
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(rotate_center_obj_.transform.position, Vector3.up, rotate_speed_ * Time.deltaTime);
+	}
+}
