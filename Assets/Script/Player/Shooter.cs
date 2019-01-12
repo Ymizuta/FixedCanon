066758155ed0c92e72c54fdf1b18ab255b179e07
Ymizuta <suiden.53@gmail.com>
2019-01-13 00:09:32 +0900

    [update]Shooter コメント追記

--- Assets/Script/Player/Shooter.cs
+++ Assets/Script/Player/Shooter.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-    private GameObject muzzle_ = null;       //砲弾を生成・発射するオブジェクト
-    public System.Action OnBulletDye;
+    /// <summary>
+    /// ・Playerクラスが保持
+    /// ・砲弾発射を行うクラス
+    /// </summary>
 
+    private GameObject muzzle_ = null;  //砲弾を生成・発射するオブジェクト
+
+    /*
+     * @ brief      StageMainStateから呼び出され、引数で渡された砲弾オブジェクトをRigidBody.AddForceで飛ばす処理
+     * @ param      bullet_clone    StageMainStateから渡される砲弾オブジェクト
+    */
     public void Shoot(BulletBase bullet_clone)
     {
-        //ゲームオブジェクトの有無をチェック
-        if (muzzle_ == null) { muzzle_ = GameObject.Find("Muzzle"); }
-
-        Debug.Log(bullet_clone + "を発射");
-        //BulletBase bullet_clone = Instantiate(bullet, muzzle_.transform.position, muzzle_.transform.rotation);
         bullet_clone.GetComponent<Rigidbody>().AddForce(muzzle_.transform.forward * bullet_clone.ShootPower);
-        //Object.Destroy(bullet_clone.gameObject, 2.0f);
         InstantiateEffect();
     }
 
-    //メインステート初期化時に取得
+    /*
+     * @ brief      StageInitStateでの初期化時に呼び出される
+    */
     public GameObject Muzzle
     {
         get
@@ -32,15 +36,16 @@ public class Shooter : MonoBehaviour {
         }
     }
 
+    /*
+     * @ brief      Muzzleの位置にエフェクトを生成する
+    */
     private void InstantiateEffect()
     {
         GameObject effect = Instantiate(Resources.Load("Effect/ShootEffect"),muzzle_.transform.position,muzzle_.transform.rotation) as GameObject;
         Destroy(effect, 2.0f);
     }
 
-    private void OnDestroy()
-    {
-        muzzle_ = null;
-        OnBulletDye = null;
-    }
+    /*
+     * @ brief      参照オブジェクトのメモリ開放
+    */
 }
