4e437ac978b59f120b4053e4b980a64cf3438443
Ymizuta <suiden.53@gmail.com>
2019-01-13 00:50:43 +0900

    [update]BulletBase コメント追記・不要なコメントアウトを削除

--- Assets/Script/Bullet/BulletType/BulletBase.cs
+++ Assets/Script/Bullet/BulletType/BulletBase.cs
@@ -3,34 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class BulletBase : MonoBehaviour {
-    protected float damage_;
-    protected float shoot_power_ = 1000.0f;
-    protected float destroy_interval_time_ = 2.0f;
-    //public System.Action OnBulletDie;
 
-    public void Init()
-    {
+    /// <summary>
+    /// 砲弾の着弾時の挙動、RididBodyで飛ばされる勢い等を設定する抽象クラス
+    /// サブクラスを砲弾オブジェクトにアタッチして利用する
+    /// </summary>
 
-    }
+    protected float damage_;                        //砲弾のダメージを設定
+    protected float shoot_power_ = 1000.0f;         //RigidBody.Addで加えられる力
+    protected float destroy_interval_time_ = 2.0f;  //着弾後にDestroyされるまでのインターバル時間
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.tag == StageObjectList.NormalObject)
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //    if (other.tag == StageObjectList.TargetObject)
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //}
+    //サブクラスにて初期化する固有の処理があれば実装
+    public void Init(){}
 
+    /*
+    * @ brief      着弾時の挙動。着弾した際の挙動は、各種砲弾で統一する。
+    */
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == StageObjectList.NormalObject)
         {
             Destroy(this.gameObject);
-            //Destroy(this.gameObject, destroy_interval_time_);
             InstantiateEffect();
         }
         else
@@ -46,16 +39,14 @@ public abstract class BulletBase : MonoBehaviour {
         }
     }
 
-    //private void OnDestroy()
-    //{
-    //    if (OnBulletDie != null)
-    //    {
-    //        OnBulletDie();
-    //    }
-    //}
-
+    /*
+    * @ brief      着弾時のエフェクト生成。サブクラスの砲弾で固有のエフェクトを実装させる。
+    */
     protected abstract void InstantiateEffect();
 
+    /*
+    * @ brief      （現時点では未実装）オブジェクトにライフ等を設定する場合に砲弾のダメージを取得させる
+    */
     public float Damage
     {
         get
@@ -68,6 +59,10 @@ public abstract class BulletBase : MonoBehaviour {
         }
     }
 
+    /*
+    * @ brief      StageMainStateが取得し、Shooterクラスに渡すことで砲弾を飛ばす処理に利用される
+    * @ detail     ShooterクラスShootメソッド内のRigidBody.Addメソッド処理にて利用
+    */
     public float ShootPower
     {
         get
@@ -79,5 +74,4 @@ public abstract class BulletBase : MonoBehaviour {
             shoot_power_ = value;
         }
     }
-
 }
