674ff7ea3dbe724de9d1973017f21d3bf20c1a84
Ymizuta <suiden.53@gmail.com>
2019-01-13 01:11:23 +0900

    [update]BulletExplosion コメント追記

--- Assets/Script/Bullet/BulletType/BulletAction/BulletExplosion.cs
+++ Assets/Script/Bullet/BulletType/BulletAction/BulletExplosion.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class BulletExplosion : MonoBehaviour {
 
-    [SerializeField] private float explosion_force_ = 500.0f;
-    [SerializeField] private float explosion_radius_ = 5.0f;
-    [SerializeField] private float up_wards_modifire = 3f;
+    /// <summary>
+    /// 砲弾オブジェクにアタッチして利用
+    /// 爆発弾：着弾した位置から一定範囲のオブジェクトを吹き飛ばす処理を追加する
+    /// </summary>
 
+    [SerializeField] private float explosion_force_ = 500.0f;   //周囲のオブジェクトを吹き飛ばす威力(エディターから調整可能)
+    [SerializeField] private float explosion_radius_ = 5.0f;    //着弾位置からの半径・範囲内のオブジェクトを吹き飛ばす（エディターから調整可能）
+    [SerializeField] private float up_wards_modifire = 3f;      //上方へオブジェクトの浮かせる力（エディターから調整可能）
+
+    /*
+    * @ brief   砲弾が接触したオブジェクトがNormalObjectなら周囲一転範囲のオブジェクトを吹き飛ばす
+    */
     private void OnCollisionEnter(Collision collision)
     {
         if ((IsStageObject(collision)))
         {
+            //着弾位置から一定半径のオブジェクト取得
             Collider[] cols = Physics.OverlapSphere(this.gameObject.transform.position, explosion_radius_);
+            //取得したオブジェクト吹き飛ばす
             foreach (Collider target in cols)
             {
+                //RigidBodyを持つオブジェクトのみ対象に吹き飛ばす処理を実施
                 if (target.GetComponent<Rigidbody>() != null)
                 {
                     Rigidbody rigidbody = target.GetComponent<Rigidbody>();
@@ -24,9 +35,13 @@ public class BulletExplosion : MonoBehaviour {
         }
     }
 
+    /*
+    * @ brief   砲弾が着弾したオブジェクトがNormalObjetかどうかを判定
+    * @ param   collision   着弾・衝突したオブジェクトから渡される
+    */
     private bool IsStageObject(Collision collision)
     {
-        if (collision.gameObject.tag == StageObjectList.NormalObject || collision.gameObject.tag == StageObjectList.NormalObject)
+        if (collision.gameObject.tag == StageObjectList.NormalObject)
         {
             return true;
         }
